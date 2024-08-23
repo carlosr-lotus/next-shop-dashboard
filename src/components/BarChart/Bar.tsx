8b40c38764a4ce6949ad244b481d394d81998f5b
Carlos Lotus <carlosr_juniorpro@protonmail.com>
2024-08-23 00:52:52 -0300

    ADD: bar chart animation

--- src/components/BarChart/Bar.tsx
+++ src/components/BarChart/Bar.tsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react"
+
 import styles from "@/styles/components/barChart/Bar.module.css"
 
 type Props = {
@@ -6,13 +8,19 @@ type Props = {
 }
 
 export default function Bar(props: Props): JSX.Element {
+
+    const [value, setValue] = useState('0%')
+
+    useEffect(() => { setValue(`${props.value}%`) }, [props.value])
+
     return (
        <>
             <div 
                 className={styles.bar}
                 style={{
                     backgroundColor: props.color,
-                    height: `${props.value}%`
+                    height: value,
+                    transition: 'height 0.2s ease-in-out'
                 }}
             ></div>
        </> 
