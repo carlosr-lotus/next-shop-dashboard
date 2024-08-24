7f24111e8c7d05951f43892ed017eb8de70f1b15
Carlos Lotus <carlosr_juniorpro@protonmail.com>
2024-08-23 22:29:27 -0300

    ADD: use props value

--- src/components/TotalPercentage.tsx
+++ src/components/TotalPercentage.tsx
@@ -12,7 +12,7 @@ export default function TotalPercentage(props: Props): JSX.Element {
                 <p>Seu negócio está mais próspero que 50% das empresas do ramo.</p>
             </div> 
             <div className={styles.area}>
-                <svg className={styles.waves} xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+                <svg className={styles.waves} style={{maxHeight: `${props.value}%`}} xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
                     <defs>
                         <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
                     </defs>
