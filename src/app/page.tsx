c95a7f7189fc367843f0dee8e96b24aaa4b1085f
Carlos Lotus <carlosr_juniorpro@protonmail.com>
2024-08-25 16:49:25 -0300

    DEL: inline style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -79,12 +79,6 @@ export default function Home() {
 
                 <div
                     className={styles.charts}
-                    // style={{
-                    //     display: 'grid',
-                    //     gridTemplateColumns: '1fr .5fr',
-                    //     gridTemplateRows: '1fr 1fr',
-                    //     gap: '1rem'
-                    // }}
                 >
                     <ChartPanel values={barChartData} />
                     <div style={{
